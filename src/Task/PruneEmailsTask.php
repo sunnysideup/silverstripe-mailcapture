81113403b98c29ed2c83640f130d9a09f1c74739
Nicolaas @ Tappy <nt@sunnysideup.co.nz>
2026-05-04 14:37:04 +1200

    API:  upgrade to new version of Silverstripe - step: Fix build tasks for Silverstripe 6.

--- src/Task/PruneEmailsTask.php
+++ src/Task/PruneEmailsTask.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Sunnysideup\MailCapture\BuildTask;
 
-use Symfony\Component\Console\Input\InputInterface;
-use SilverStripe\PolyExecution\PolyOutput;
-use Symfony\Component\Console\Command\Command;
 use SilverStripe\Dev\BuildTask;
+use SilverStripe\PolyExecution\PolyOutput;
 use SilverStripe\Security\Permission;
 use Sunnysideup\MailCapture\Model\CapturedEmail;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * @author <marcus@silverstripe.com.au>
@@ -17,15 +18,38 @@ use Sunnysideup\MailCapture\Model\CapturedEmail;
  */
 class PruneEmailsTask extends BuildTask
 {
+    protected static string $commandName = 'prune-emails';
+
+    protected string $title = 'Prune old captured emails';
+
+    protected static string $description = 'Deletes captured emails older than 1 month';
+
     protected function execute(InputInterface $input, PolyOutput $output): int
     {
-        if (Permission::check('ADMIN')) {
-            $since = date('Y-m-d H:i:s', strtotime('-1 month'));
-            $list = CapturedEmail::get()->filter(['Created:LessThan' => $since]);
-            echo "Deleting " . $list->count() . " captured emails (if ?confirm get var is set)<br/>\n";
+        if (!Permission::check('ADMIN')) {
+            $output->writeln('<error>You must be an admin to run this task</error>');
+            return Command::FAILURE;
+        }
+
+        $since = date('Y-m-d H:i:s', strtotime('-1 month'));
+        $list = CapturedEmail::get()->filter(['Created:LessThan' => $since]);
+        $count = $list->count();
+
+        if ($input->getOption('confirm')) {
+            $output->writeln("Deleting {$count} captured emails...");
             $list->removeAll();
+            $output->writeln('<info>Emails deleted successfully</info>');
+        } else {
+            $output->writeln("Would delete {$count} captured emails. Use --confirm to actually delete them.");
         }
 
         return Command::SUCCESS;
+    }
+
+    public function getOptions(): array
+    {
+        return [
+            new InputOption('confirm', 'c', InputOption::VALUE_NONE, 'Confirm deletion of emails'),
+        ];
     }
 }
